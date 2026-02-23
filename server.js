c1567afdd23a172bdc1482a7074cdffb82e1ad8a
Bhuwan Bhashkar <nigamrai666@gmail.com>
2026-02-23 23:39:01 +0530

    updating server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
